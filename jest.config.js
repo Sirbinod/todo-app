d451973422d65fe2cf46cb9c41aaefeceffd0c3a
Sirbinod <00np10@gmail.com>
2024-03-01 02:53:28 +0545

    feat: todo test

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  testMatch: ["**/__tests__/**/*.spec.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
+  },
+};
